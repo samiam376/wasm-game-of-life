f771849bff9c10e10bc83c606264e3fc47a1e5e8
samiam376 <ssheridan211@gmail.com>
2022-11-08 22:39:18 -0800

    timing

--- src/lib.rs
+++ src/lib.rs
@@ -19,6 +19,23 @@ macro_rules! log {
     }
 }
 
+pub struct Timer<'a> {
+    name: &'a str,
+}
+
+impl<'a> Timer<'a> {
+    pub fn new(name: &'a str) -> Timer<'a> {
+        web_sys::console::time_with_label(name);
+        Timer { name }
+    }
+}
+
+impl<'a> Drop for Timer<'a> {
+    fn drop(&mut self) {
+        web_sys::console::time_end_with_label(self.name);
+    }
+}
+
 #[wasm_bindgen]
 #[repr(u8)]
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
@@ -50,21 +67,58 @@ impl Universe{
         (row * self.width + column) as usize
     }
 
-    fn live_neighbor_count(&self, row: u32, column: u32) -> u8{
+
+    fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
         let mut count = 0;
-
-        for delta_row in [self.height - 1, 0, 1].iter().cloned(){
-            for delta_col in [self.width - 1, 0, 1].iter().cloned(){
-                if delta_row == 0 && delta_col == 0{
-                    continue;
-                }
-
-                let neighbor_row = (row + delta_row) % self.height;
-                let neighbor_col = (column + delta_col) % self.width;
-                let idx = self.get_index(neighbor_row, neighbor_col);
-                count += self.cells[idx] as u8
-            }
-        }
+    
+        let north = if row == 0 {
+            self.height - 1
+        } else {
+            row - 1
+        };
+    
+        let south = if row == self.height - 1 {
+            0
+        } else {
+            row + 1
+        };
+    
+        let west = if column == 0 {
+            self.width - 1
+        } else {
+            column - 1
+        };
+    
+        let east = if column == self.width - 1 {
+            0
+        } else {
+            column + 1
+        };
+    
+        let nw = self.get_index(north, west);
+        count += self.cells[nw] as u8;
+    
+        let n = self.get_index(north, column);
+        count += self.cells[n] as u8;
+    
+        let ne = self.get_index(north, east);
+        count += self.cells[ne] as u8;
+    
+        let w = self.get_index(row, west);
+        count += self.cells[w] as u8;
+    
+        let e = self.get_index(row, east);
+        count += self.cells[e] as u8;
+    
+        let sw = self.get_index(south, west);
+        count += self.cells[sw] as u8;
+    
+        let s = self.get_index(south, column);
+        count += self.cells[s] as u8;
+    
+        let se = self.get_index(south, east);
+        count += self.cells[se] as u8;
+    
         count
     }
 
@@ -127,6 +181,7 @@ impl  Universe {
     }
 
     pub fn tick(&mut self) {
+        let _timer = Timer::new("Universe::tick");
         let mut next = self.cells.clone();
 
         for row in 0..self.height{
@@ -135,14 +190,6 @@ impl  Universe {
                 let cell = self.cells[idx];
                 let live_neighbors = self.live_neighbor_count(row, col);
 
-                
-               log!(
-                        "cell[{}, {}] is initially {:?} and has {} live neighbors",
-                        row,
-                        col,
-                        cell,
-                        live_neighbors
-                    );
 
                 let next_cell = match (cell, live_neighbors) {
                     (Cell::Alive, x) if x < 2 =>  Cell::Dead,
@@ -152,7 +199,6 @@ impl  Universe {
                     (otherwise, _) => otherwise
                 };
                 
-                log!("    it becomes {:?}", next_cell);
                 next[idx] = next_cell;
 
             }
